38ad4167fe642820cca9d49e851b5f75603ba256
Albert <wissigkeit@outlook.de>
2023-05-08 16:22:38 +0200

    added question and small card design

---
+++ script.js
@@ -0,0 +1,82 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",   //definiert als sogenannten string  (text-buchstabenfolge)
+        "right_answer_3": 3  // definiert als sogenannter integer (zahl)
+    },
+    {
+        "question": "Was kann man vom Mond aus sehen?",
+        "answer_1": "Chinesische Mauer",
+        "answer_2": "Freiheitsstatue",
+        "answer_3": "Eiffelturm",
+        "answer_4": "Pentagon",
+        "right_answer_1": 1
+    },
+    {
+        "question": "Was ist eine Platane?",
+        "answer_1": "Laubbaum",
+        "answer_2": "Abdeckung",
+        "answer_3": "Computerbauteil",
+        "answer_4": "grosser Bauernhof",
+        "right_answer_1": 1
+    },
+    {
+        "question": "Wie viele Igelarten gibt es Weltweit?",
+        "answer_1": "99",
+        "answer_2": "26",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer_1": 2
+    },
+    {
+        "question": "Wie viele stacheln hat ein ausgewachsener Igel?",
+        "answer_1": "1.000-2.500",
+        "answer_2": "5.000-8.000",
+        "answer_3": "10.000-12.500",
+        "answer_4": "12.500-14.000",
+        "right_answer_1": 2
+    },
+    {
+        "question": "Wer war keiner der Heiligen drei Könige aus der Bibelgeschichte?",
+        "answer_1": "Caspar",
+        "answer_2": "Melchor",
+        "answer_3": "Hosea",
+        "answer_4": "Balthasar",
+        "right_answer_1": 3
+    },
+    {
+        "question": "Wie wird ein explodierender Stern genannt?",
+        "answer_1": "Supernova",
+        "answer_2": "SETI",
+        "answer_3": "Big Bang",
+        "answer_4": "Roter Riese",
+        "right_answer_1": 1
+    },
+    {
+        "question": "Wie heißt die Nachbargalaxie der Milchstraße?",
+        "answer_1": "Fornax",
+        "answer_2": "Ogarmega",
+        "answer_3": "Andromeda",
+        "answer_4": "Verdan",
+        "right_answer_1":  3
+    },
+    {
+        "question": "Welche Farbe haben Khao Manee-Katzen ausschließlich?",
+        "answer_1": "blau",
+        "answer_2": "schwarz",
+        "answer_3": "braun",
+        "answer_4": "weiß",
+        "right_answer_1": 4 
+    },
+    {
+        "question": "Zu welchem Genre gehört das Spiel \"World of Warcraft\"?",
+        "answer_1": "MMO",
+        "answer_2": "MMORPG",
+        "answer_3": "SRPG",
+        "answer_4": "FPS",
+        "right_answer_1":  2
+    },
+];
